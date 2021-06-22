974a2ffd920e6b70f42f1c67d3899551672bd41e
lu_weicong <lu_weicong@purvar.co.jp>
2021-06-22 09:23:27 +0900

    i18n added

--- src/main/java/com/example/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/example/demo/config/MyWebMvcConfigurer.java
@@ -1,6 +1,14 @@
 package com.example.demo.config;
 
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -23,6 +31,30 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 		WebMvcConfigurer.super.addInterceptors(registry);
 	}
 	
-	
-	
+	/**
+	 * 国际化，自定义实现LocaleResolver
+	 * 
+	 * @return
+	 */
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new LocaleResolver() {
+			@Override
+			public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+			}
+
+			@Override
+			public Locale resolveLocale(HttpServletRequest request) {
+				String lang = request.getParameter("lang"); // zh_CN en_US ja_JP
+				lang = "zh_CN";
+				Locale locale = request.getLocale();
+				if (StringUtils.hasLength(lang)) {
+					String[] localeArray = lang.split("_");
+					locale = new Locale(localeArray[0], localeArray[1]);
+				}
+				return locale;
+			}
+		};
+	}
+
 }
